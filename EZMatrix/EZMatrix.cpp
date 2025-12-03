647f7a7df18bd473ce49dfd6a53c1868ebca298d
mugen <thoaramail@gmail.com>
2025-12-03 19:30:46 +0100

    FIXES VERSION

--- EZMatrix/EZMatrix.cpp
+++ EZMatrix/EZMatrix.cpp
@@ -4,7 +4,7 @@ EZMatrix::EZMatrix(LedControl &controller)
   : lc(controller) {}
 
 void EZMatrix::begin(uint8_t intensity) {
-  lc.shutdown(0, false);
+  lc.shutdown(0, false);      // turn on display
   lc.setIntensity(0, intensity);
   lc.clearDisplay(0);
 }
@@ -13,15 +13,23 @@ void EZMatrix::clear() {
   lc.clearDisplay(0);
 }
 
-// auto-rotated 90 degrees clockwise
+// Draw frame rotated 90° clockwise to face DOWN
 void EZMatrix::draw(byte grid[8][8]) {
-  for (int r = 0; r < 8; r++) {
+  for (int r = 0; r < 8; r++) {           // target row
     byte rowByte = 0;
-    for (int c = 0; c < 8; c++) {
-      // rotate: new[r][c] = old[7-c][r]
-      byte pixel = grid[7 - c][r];
-      rowByte |= (pixel << (7 - c));
+    for (int c = 0; c < 8; c++) {         // source column
+      rowByte |= (grid[7 - c][r] << (7 - c));  // rotate 90° clockwise
     }
     lc.setRow(0, r, rowByte);
+  }
+}
+
+// Animate frames
+void EZMatrix::animate(byte* frames[], uint8_t numFrames, uint16_t delayMs, uint8_t repeats) {
+  for (uint8_t r = 0; r < repeats; r++) {
+    for (uint8_t f = 0; f < numFrames; f++) {
+      draw((byte (*)[8])frames[f]);  // convert pointer back to 2D array
+      delay(delayMs);
+    }
   }
 }
--- EZMatrix/EZMatrix.h
+++ EZMatrix/EZMatrix.h
@@ -7,9 +7,12 @@
 class EZMatrix {
 public:
   EZMatrix(LedControl &controller);
-  void begin(uint8_t intensity = 8);
-  void draw(byte grid[8][8]);
-  void clear();
+
+  void begin(uint8_t intensity = 8);                  // initialize matrix
+  void draw(byte grid[8][8]);                         // draw a single frame
+  void clear();                                       // clear display
+  void animate(byte* frames[], uint8_t numFrames,    // animate multiple frames
+               uint16_t delayMs, uint8_t repeats = 255);
 
 private:
   LedControl &lc;
